8cbd53c4ae8f282a415888839905f64f75f784b1
markyMARK <>
2021-05-12 19:29:21 -0400

    Updated route names

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/discography">Discography</router-link> |
-    <router-link to="/events">Events</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Discography' }">Discography</router-link> |
+    <router-link :to="{ name: 'Events' }">Events</router-link>
   </div>
 </template>
 
