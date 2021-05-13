e54ef3f47170f936a6027d6ec5eefc6f3c090de8
markyMARK <>
2021-05-13 19:19:29 -0400

    Discography songs

--- src/views/Discography.vue
+++ src/views/Discography.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="discography">
-    <h1>This is the Discography page</h1>
+    <h1>U P P E R C A S E</h1>
+    <hr width="50%" align="center" />
+    <h2>Discography</h2>
+    <p v-for="release in discography.releases" :key="release.title">
+      <span
+        ><b>{{ release.title }}</b></span
+      ><br />
+      <span>Released: {{ release.releaseDate }}</span
+      ><br />
+      <a :href="release.spotifyUrl" target="_blank">Listen on Spotify</a>
+    </p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import store from '@/store/index.js'
+
+function getReleases(next) {
+  // call module action
+  store
+    .dispatch('discography/fetchReleases')
+    .then(() => {
+      next()
+    })
+    .catch((error) => {
+      if (error.response && error.response.status == 404) {
+        // redirect to 404 page with name of resource missing
+        next({ name: '404', params: { resource: 'page' } })
+      } else {
+        next({ name: 'NetworkIssue' })
+      }
+    })
+}
+
+export default {
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    getReleases(next)
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    getReleases(next)
+  },
+  computed: {
+    // map store module and expose to component as data
+    ...mapState(['discography'])
+  }
+}
+</script>
