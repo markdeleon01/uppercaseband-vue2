9317d79b9d5e31788297d33fa7670916ccb049f3
markyMARK <>
2021-05-18 22:46:11 -0400

    Responsive styling with media queries

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -54,7 +54,83 @@ export default {
 <style scoped>
 .event-item {
   padding-top: 40px;
-  padding-left: 140px;
-  padding-right: 140px;
+}
+@media (min-width: 330px) {
+  .event-item {
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+}
+@media (min-width: 405px) {
+  .event-item {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+}
+@media (min-width: 450px) {
+  .event-item {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+}
+@media (min-width: 495px) {
+  .event-item {
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+}
+@media (min-width: 710px) {
+  .event-item {
+    padding-left: 60px;
+    padding-right: 60px;
+  }
+}
+@media (min-width: 730px) {
+  .event-item {
+    padding-left: 70px;
+    padding-right: 70px;
+  }
+}
+@media (min-width: 750px) {
+  .event-item {
+    padding-left: 80px;
+    padding-right: 80px;
+  }
+}
+@media (min-width: 770px) {
+  .event-item {
+    padding-left: 90px;
+    padding-right: 90px;
+  }
+}
+@media (min-width: 790px) {
+  .event-item {
+    padding-left: 100px;
+    padding-right: 100px;
+  }
+}
+@media (min-width: 800px) {
+  .event-item {
+    padding-left: 120px;
+    padding-right: 120px;
+  }
+}
+@media (min-width: 850px) {
+  .event-item {
+    padding-left: 150px;
+    padding-right: 150px;
+  }
+}
+@media (min-width: 900px) {
+  .event-item {
+    padding-left: 180px;
+    padding-right: 180px;
+  }
+}
+@media (min-width: 1024px) {
+  .event-item {
+    padding-left: 200px;
+    padding-right: 200px;
+  }
 }
 </style>
